d9e99e254bbf7fb84d43bb70bc6473f0b9ede868
MertSaygili <mert71719601@gmail.com>
2022-08-31 18:51:20 +0300

    default card almost done

--- lib/ui/widgets/custom_card.dart
+++ lib/ui/widgets/custom_card.dart
@@ -37,43 +37,82 @@ class _CustomCardState extends State<CustomCard> {
 
   Widget _checkTitle() {
     if (widget.title == null) {
-      return Padding(
-        padding: PaddignItems().paddingInsideCard,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              widget.subtitle,
-              style: Theme.of(context).textTheme.headline1,
-            ),
-            const StackButton(),
-          ],
-        ),
-      );
+      return _CardWithoutTitle(subtitle: widget.subtitle);
     }
+    return _CardWithTitle(title: widget.title ?? '', subtitle: widget.subtitle);
+  }
+}
+
+class _CardWithTitle extends StatelessWidget {
+  const _CardWithTitle({
+    Key? key,
+    required this.title,
+    required this.subtitle,
+  }) : super(key: key);
+
+  final String title;
+  final String subtitle;
+
+  @override
+  Widget build(BuildContext context) {
     return Padding(
       padding: PaddignItems().paddingInsideCard,
       child: Column(
         children: [
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              widget.title ?? '',
-              style: Theme.of(context).textTheme.subtitle1,
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                widget.subtitle,
-                style: Theme.of(context).textTheme.headline1,
-              ),
-              const StackButton(),
-            ],
-          ),
+          _title(context),
+          _SubBlock(subtitle: subtitle),
         ],
       ),
+    );
+  }
+
+  Align _title(BuildContext context) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.subtitle1,
+      ),
+    );
+  }
+}
+
+class _CardWithoutTitle extends StatelessWidget {
+  const _CardWithoutTitle({
+    Key? key,
+    required this.subtitle,
+  }) : super(key: key);
+
+  final String subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: PaddignItems().paddingInsideCard,
+      child: _SubBlock(subtitle: subtitle),
+    );
+  }
+}
+
+class _SubBlock extends StatelessWidget {
+  const _SubBlock({
+    Key? key,
+    required this.subtitle,
+  }) : super(key: key);
+
+  final String subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          subtitle,
+          style: Theme.of(context).textTheme.headline1,
+        ),
+        const StackButton(),
+      ],
     );
   }
 }
