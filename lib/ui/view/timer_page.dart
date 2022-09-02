ba34688deb5df5bba4d26d791d1f86453a681ed5
MertSaygili <mert71719601@gmail.com>
2022-09-02 17:53:08 +0300

    added next step as an commit line

--- lib/ui/view/timer_page.dart
+++ lib/ui/view/timer_page.dart
@@ -15,43 +15,59 @@ class TimerPageView extends StatefulWidget {
 }
 
 class _TimerPageViewState extends State<TimerPageView> {
+  //todo:: zamani al ve elevated button icinde kullan
+  late int _hour;
+  late int _minute;
+  late int _second;
+
   final double _prefferedSize = 50;
   final String _textSaat = 'Saat';
   final String _textDakika = 'Dakika';
   final String _textSaniye = 'Saniye';
-  int _currentValue = 0;
+  final String _textBaslat = 'Baslat';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: CustomSettingsAppBar(
-          prefferedSize: _prefferedSize,
-          customIconButton: CustomIconButton(
-            icon: IconItems().addIcon,
-            fun: null,
-          ),
-          customIconButton2: CustomIconButton(
-            icon: IconItems().settingsIcon,
-            fun: null,
-          ),
+      appBar: CustomSettingsAppBar(
+        prefferedSize: _prefferedSize,
+        customIconButton: CustomIconButton(
+          icon: IconItems().addIcon,
+          fun: null,
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                _TimerColumn(text: _textSaat, minValue: 0, maxValue: 99),
-                _TimerColumn(text: _textDakika, minValue: 0, maxValue: 59),
-                _TimerColumn(text: _textSaniye, minValue: 0, maxValue: 59),
-              ],
-            )
-          ],
-        ));
+        customIconButton2: CustomIconButton(
+          icon: IconItems().settingsIcon,
+          fun: null,
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              _TimerColumn(text: _textSaat, minValue: 0, maxValue: 99),
+              _TimerColumn(text: _textDakika, minValue: 0, maxValue: 59),
+              _TimerColumn(text: _textSaniye, minValue: 0, maxValue: 59),
+            ],
+          ),
+          //todo:: elevated buttonu widget olarak cikar islemleri orada yap
+          ElevatedButton(onPressed: () {}, child: Text(_textBaslat)),
+        ],
+      ),
+    );
+  }
+
+  void setTimeProperties(int hour, int minute, int second) {
+    setState(() {
+      _hour = hour;
+      _minute = minute;
+      _second = second;
+    });
   }
 }
 
-class _TimerColumn extends StatelessWidget {
+class _TimerColumn extends StatefulWidget {
   const _TimerColumn({
     Key? key,
     required this.text,
@@ -64,12 +80,33 @@ class _TimerColumn extends StatelessWidget {
   final int maxValue;
 
   @override
+  State<_TimerColumn> createState() => _TimerColumnState();
+}
+
+class _TimerColumnState extends State<_TimerColumn> {
+  late int timeVal;
+
+  //todo:: zamani aldik ama hangisinin hangisi oldugu bilmiyoruz
+  //! text'e sahip oldugumuz icin hangisinin hangisi oldugunu anlayabiliriz ama
+  //! yeterli degil 3 farkli fonksiyon disinde tek fonks ile birtirmeye calis
+  //! ayirma islemi burada gerceklesmeli
+  void setTime(int val) {
+    setState(() {
+      timeVal = val;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        _Text(text: text),
-        _NumberPicker(minValue: minValue, maxValue: maxValue),
+        _Text(text: widget.text),
+        _NumberPicker(
+          fun: setTime,
+          minValue: widget.minValue,
+          maxValue: widget.maxValue,
+        ),
       ],
     );
   }
@@ -95,10 +132,12 @@ class _Text extends StatelessWidget {
 class _NumberPicker extends StatefulWidget {
   const _NumberPicker({
     Key? key,
+    required this.fun,
     required this.minValue,
     required this.maxValue,
   }) : super(key: key);
 
+  final Function fun;
   final int minValue;
   final int maxValue;
 
@@ -119,7 +158,12 @@ class _NumberPickerState extends State<_NumberPicker> {
       minValue: widget.minValue,
       maxValue: widget.maxValue,
       value: _currentValue,
-      onChanged: (value) => setState(() => _currentValue = value),
+      onChanged: (value) {
+        setState(() {
+          _currentValue = value;
+          widget.fun(value);
+        });
+      },
     );
   }
 }
